d685c16fd625e60a056b312c6d43951aa7264d36
AssanSav <assane.savadogo81@gmail.com>
2020-02-10 20:47:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 function fetchBooks() {
-
+  let getApi = fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(json => renderBooks(json));
+  return getApi
 }
 
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
+    h2.innerHTML = `<h1>Author: ${book.authors}<h1><h2>${book.name}</h2>`
     main.appendChild(h2)
   })
 }
